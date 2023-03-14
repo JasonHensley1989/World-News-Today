f9657f7557a587d28db8da5a8beee1cd3b489ff7
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-14 17:36:05 -0500

    styling updates, next is search implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import NewsList from "./components/NewsList";
+import TitleArea from "./components/TitleArea";
 
 function App() {
   return (
     <div className="App">
+      <TitleArea />
       <NewsList />
     </div>
   );
---
+++ src/components/TitleArea.js
@@ -0,0 +1,10 @@
+const TitleArea = () => {
+  return (
+    <>
+        <h1 className="title">World News Today</h1>
+        <p className="title">Simply the Best</p>
+    </>
+  )
+}
+
+export default TitleArea
