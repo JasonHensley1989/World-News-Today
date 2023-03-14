69a1f7394c6a9b21c1cb3d2aceca428a8601b8bf
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-14 17:51:19 -0500

    search bar creation needs state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NewsList from "./components/NewsList";
 import TitleArea from "./components/TitleArea";
 
 function App() {
+  
   return (
     <div className="App">
       <TitleArea />
--- src/components/TitleArea.js
+++ src/components/TitleArea.js
@@ -1,14 +1,19 @@
-const TitleArea = () => {
-  return (
-    <>
-        <h1 className="title">World News Today</h1>
-        <p className="title">Simply the Best</p>
-        <div className="searchBar">
-            <input  placeholder="What are you looking for" className="search" />
-            <button type="submit" className="search search-cursor" >Search</button>
-        </div>
-    </>
-  )
+import { useState } from "react"
+
+const SearchArea = () => {
+    const [ currentSearch, setCurrentSearch ] = useState()
+
+
+    return (
+        <>
+            <h1 className="title">World News Today</h1>
+            <p className="title">Simply the Best</p>
+            <div className="searchBar">
+                <input  placeholder="What are you looking for" className="search" />
+                <button type="submit" className="search search-cursor" >Search</button>
+            </div>
+        </>
+    )
 }
 
-export default TitleArea
+export default SearchArea
